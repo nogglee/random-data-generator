6fa7e1c01c5971a9e776c7588c5f0bfb47ce39b3
nogglee <dev.nogglee@gmail.com>
2026-03-06 22:37:12 +0900

    feat: 프로젝트 셋업 완료

---
+++ random-user.js
@@ -0,0 +1,19 @@
+import { faker } from '@faker-js/faker';
+
+export function createRandomUser() {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.username(),
+    email: faker.internet.email(),
+    avatar: faker.image.avatar(),
+    password: faker.internet.password(),
+    birthdate: faker.date.birthdate(),
+    registeredAt: faker.date.past(),
+  };
+}
+
+export const users = faker.helpers.multiple(createRandomUser, {
+  count: 5,
+});
+
+console.log(users)
